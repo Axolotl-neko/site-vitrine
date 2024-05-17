2be691acf8a7317e2c993410faae618ae3f53999
Rayane nacib <94367122+Axolotl-neko@users.noreply.github.com>
2024-05-17 10:27:50 +0100

    Add files via upload

---
+++ front-end/assets/js/script.js
@@ -0,0 +1,123 @@
+
+
+$( document ).ready(function() {
+
+     $('#slider1').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 5000,
+        navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+      $('#slider2').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 5000,
+        navText:['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+
+
+});
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+
+
+//le compteur de stat
+
+
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 3000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    if (startValue ==1000){startValue = endValue; }
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
