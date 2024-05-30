52c3ef87960a234c4d1bd57cd93cfe19119f7c7a
Rayane nacib <94367122+Axolotl-neko@users.noreply.github.com>
2024-05-30 02:46:09 +0100

    Add files via upload

--- front-end/assets/js/script.js
+++ front-end/assets/js/script.js
@@ -1,5 +1,21 @@
 
 
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId),
+         nav = document.getElementById(navId)
+
+   toggle.addEventListener('click', () =>{
+       // Add show-menu class to nav menu
+       nav.classList.toggle('show-menu')
+
+       // Add show-icon to show and hide the menu icon
+       toggle.classList.toggle('show-icon')
+   })
+}
+
+showMenu('nav-toggle','nav-menu')
+
 $( document ).ready(function() {
 
      $('#slider1').owlCarousel({
@@ -96,7 +112,7 @@ valueDisplays.forEach((valueDisplay) => {
   let duration = Math.floor(interval / endValue);
   let counter = setInterval(function () {
     startValue += 1;
-    if (startValue ==1000){startValue = endValue; }
+    if (startValue ==5000){startValue = endValue; }
     valueDisplay.textContent = startValue;
     if (startValue == endValue) {
       clearInterval(counter);
